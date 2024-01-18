172d2498675ccbe5bb8834260caa34f93ee5b72b
crackkhe <157005065+crackkhe@users.noreply.github.com>
2024-01-18 06:20:37 -0500

    Revert "Update server.js"
    This reverts commit 16b89348e8bd921a754e122dc10ff2e4a42c195d.

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(cors());
 // Resto de la configuración del servidor...
 
 // Inicia el servidor
-app.listen(8000, () => {
+app.listen(7000, () => {
   console.log('El servidor está escuchando en el puerto 8000');
 });
