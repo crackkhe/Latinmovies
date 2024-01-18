56e240d0d937c6bd23de8ae84a9172676f6c1fee
crackkhe <157005065+crackkhe@users.noreply.github.com>
2024-01-18 05:05:25 -0500

    update

--- addon.js
+++ addon.js
@@ -1,68 +1,54 @@
 const { addonBuilder } = require("stremio-addon-sdk");
 const magnet = require("magnet-uri");
-const cors = require('cors');  // Agrega esta línea para importar el módulo CORS
-
 
 const manifest = { 
-    "id": "latinomovies",
-    "version": "13.2.0",
+    "id": "org.stremio.helloworld",
+    "version": "1.0.0",
 
-    "name": "LatinoMovies",
-    "description": "SWatch movies in español latino ES-ES, ES-MX",
+    "name": "Hello World Addon",
+    "description": "Sample addon providing a few public domain movies",
 
-    // set what type of resources we will return
     "resources": [
         "catalog",
         "stream"
     ],
 
-    "types": ["movie", "series"], // your add-on will be preferred for those content types
-
-    // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
+    "types": ["movie", "series"],
+    
     "catalogs": [
         {
             type: 'movie',
-            id: 'LatinoMoviesmov'
+            id: 'helloworldmovies'
         },
         {
             type: 'series',
-            id: 'LatinoMoviesser'
+            id: 'helloworldseries'
         }
     ],
 
-    // prefix of item IDs (ie: "tt0032138")
     "idPrefixes": [ "tt" ]
 
 };
 
 const dataset = {
-    // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
     "tt0032138": { name: "The Wizard of Oz", type: "movie", infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0 },
     "tt0017136": { name: "Metropolis", type: "movie", infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", fileIdx: 1 },
-
-    // night of the living dead, example from magnet
     "tt0063350": fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
     "tt0051744": { name: "House on Haunted Hill", type: "movie", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
-
-    "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }, // HTTP stream
-    "tt0031051": { name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" }, // YouTube stream
-
-    "tt0137523": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com" }, // redirects
-
-    "tt0137524": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/Wet__Bunny" }, // redirects
-
-    "tt0137525": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/CrazyTori" }, // redirects
-
-    "tt0137526": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/KateJonson" }, // redirects
-
-    "tt0137527": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/dreams4u" }, // redirects
-
-    "tt0137528": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/CrazyTori" }, // redirects
-
-    "tt1748166:1:1": { name: "Pioneer One", type: "series", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }, // torrent for season 1, episode 1
+    "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" },
+    "tt0031051": { name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" },
+    "tt0137523": { name: "Fight Club", type: "movie", externalUrl: "https://www.netflix.com/watch/26004747" },
+    "tt1748166:1:1": { name: "Pioneer One", type: "series", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" },
+    
+    // Se añaden seis nuevas opciones con URLs externas
+    "tt1234567": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/CrazyTori" },
+    "tt2345678": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/Wet__Bunny" },
+    "tt3456789": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/KateJonson" },
+    "tt4567890": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/dreams4u" },
+    "tt5678901": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/CrazyTori" },
+    "tt6789012": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com" },
 };
 
-// utility function to add from magnet
 function fromMagnet(name, type, uri) {
     const parsed = magnet.decode(uri);
     const infoHash = parsed.infoHash.toLowerCase();
@@ -75,51 +61,38 @@ function fromMagnet(name, type, uri) {
         infoHash: infoHash,
         sources: (parsed.announce || []).map(function(x) { return "tracker:"+x }).concat(["dht:"+infoHash]),
         tag: tags,
-        title: tags[0], // show quality in the UI
+        title: tags[0],
     }
 }
 
 const builder = new addonBuilder(manifest);
 
-// Usa el middleware cors para permitir solicitudes desde cualquier origen
-builder.use(cors());
-
-// ... Resto del código ...
-
-
-// Streams handler
 builder.defineStreamHandler(function(args) {
     if (dataset[args.id]) {
         return Promise.resolve({ streams: [dataset[args.id]] });
     } else {
         return Promise.resolve({ streams: [] });
     }
-})
+});
 
-const METAHUB_URL = "https://images.metahub.space"
+const METAHUB_URL = "https://images.metahub.space";
 
 const generateMetaPreview = function(value, key) {
-    // To provide basic meta for our movies for the catalog
-    // we'll fetch the poster from Stremio's MetaHub
-    // see https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/meta.md#meta-preview-object
-    const imdbId = key.split(":")[0]
+    const imdbId = key.split(":")[0];
     return {
         id: imdbId,
         type: value.type,
         name: value.name,
         poster: METAHUB_URL+"/poster/medium/"+imdbId+"/img",
-    }
-}
+    };
+};
 
 builder.defineCatalogHandler(function(args, cb) {
-    // filter the dataset object and only take the requested type
     const metas = Object.entries(dataset)
-	.filter(([_, value]) => value.type === args.type)
-	.map(([key, value]) => generateMetaPreview(value, key))
+        .filter(([_, value]) => value.type === args.type)
+        .map(([key, value]) => generateMetaPreview(value, key));
 
-    return Promise.resolve({ metas: metas })
-})
+    return Promise.resolve({ metas: metas });
+});
 
-module.exports = builder.getInterface()
-
-
+module.exports = builder.getInterface();
